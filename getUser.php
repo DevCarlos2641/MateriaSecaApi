dd94a7f42f18d5ae9e78359284562e59fb7449b8
Carlos Iván <Dev.Carlos2641@Outlook.com>
2024-11-23 13:06:03 -0600

    update request and orchards

--- getUser.php
+++ getUser.php
@@ -11,6 +11,7 @@
         die("Conexión fallida: " . implode(", ", $errorInfo));
     }
 
+
     $data = json_decode(file_get_contents('php://input'));
 
     $sql = "SELECT * FROM usuario WHERE correo = ? && contraseña = ?";
@@ -31,7 +32,8 @@
 
             // get request for technician from DB and save in to  var $solis
             $idTec = $tecnico['id_tecnico'];
-            $sql = "SELECT s.id_solicitud, s.id_hue, s.id_tecnico, s.status, s.fecha_programada, s.motivo_cancelacion, s.tipo, s.rango, o.cantidad 
+            $sql = "SELECT s.id_solicitud, s.id_hue, s.id_tecnico, s.status, s.fecha_programada, s.motivo_cancelacion, s.tipo, s.rango, o.cantidad,
+                    s.floracion, s.id_folio
                     FROM solicitudes s JOIN opciones_muestras o ON s.id_opciones_muestras = o.id_opciones_muestras 
                     WHERE s.id_tecnico = ? AND status = 'activa'";
             $stm = $conn->prepare($sql);
@@ -45,14 +47,9 @@
             //  get huertas for request from DB and save in to var $huertas
             $huertas = [];
             foreach($solis as $value){
-                $hue = $value['id_hue'];
-                $sql = "SELECT * FROM huertas WHERE id_hue = ?";
-                $stm = $conn->prepare($sql);
-                $stm->execute(array($hue));
-                $huerta = $stm->fetch(PDO::FETCH_ASSOC);
-                if(!(in_array($huerta, $huertas))){
-                    array_push($huertas, $huerta);
-                }
+                $val = $value['id_hue'];
+                if($val != null) array_push($huertas, getOrchardsByHue($val));
+                else array_push($huertas, getOrchardsByFolio($value['id_folio']));
             }
 
             $juntas = [];
@@ -90,5 +87,33 @@
 
     // Devolver las opciones como respuesta (formato JSON)
     echo json_encode($options);
+
+    function getOrchardsByHue($hue){
+        global $conn;
+        global $huertas;
+        $sql = "SELECT t.*, t.id_hue as id FROM (SELECT * FROM huertas WHERE id_hue = ?) t";
+        $stm = $conn->prepare($sql);
+        $stm->execute(array($hue));
+        $huerta = $stm->fetch(PDO::FETCH_ASSOC);
+        unset($huerta["id_hue"]);
+        unset($huerta["id_folio"]);
+        if(!(in_array($huerta, $huertas))){
+            return $huerta;
+        }
+    }
+
+    function getOrchardsByFolio($fol){
+        global $conn;
+        global $huertas;
+        $sql = "SELECT t.*, t.id_folio as id FROM (SELECT * FROM huertas_foliadas WHERE id_folio = ?) t";
+        $stm = $conn->prepare($sql);
+        $stm->execute(array($fol));
+        $huerta = $stm->fetch(PDO::FETCH_ASSOC);
+        unset($huerta["id_folio"]);
+        unset($huerta["id_hue"]);
+        if(!(in_array($huerta, $huertas))){
+            return $huerta;
+        }
+    }
 
 ?>
